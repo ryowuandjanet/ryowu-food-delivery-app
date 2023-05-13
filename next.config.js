faef5be5d6ef184d2cbea3c0b3d2b1acc9d54262
ryowuandjanet <ryowu0329@gmail.com>
2023-05-13 19:46:27 +0800

    init commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        hostname: "firebasestorage.googleapis.com",
+        protocol: "https",
+        port: "",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgDwmEOpxVXAJD1SjZoVg1kuSQjl6yaUQ",
+  authDomain: "food-ordering-app-d6644.firebaseapp.com",
+  projectId: "food-ordering-app-d6644",
+  storageBucket: "food-ordering-app-d6644.appspot.com",
+  messagingSenderId: "885483480790",
+  appId: "1:885483480790:web:2aec5914803e87b9f18b77",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
